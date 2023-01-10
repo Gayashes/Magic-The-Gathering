1c2905e888bef3c94b2bd33c57e6bfc12ec0b90d
Khramklen <khramklen@gmail.com>
2023-01-10 23:10:26 +0300

    routes_main_fix_place

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,52 +1,8 @@
 const express = require('express');
 
 const route = express.Router();
-const { Card } = require('../../db/models');
-const render = require('../lib/render');
-const Main = require('../views/Main');
+const renderHome = require('../controllers/mainControllers');
 
-route.get('/', async (req, res) => {
-  // const allCard = await Card.findAll();
-  const allCard = [{
-    link: 'https://pepelsbey.net/pres/web-in-curves/pictures/hypnotoad.png',
-    title: '2',
-    status: true,
-    id: 1,
-  },
-  {
-    link: 'https://pepelsbey.net/pres/web-in-curves/pictures/hypnotoad.png',
-    title: '21',
-    status: true,
-    id: 1,
-  },
-  {
-    link: 'https://pepelsbey.net/pres/web-in-curves/pictures/hypnotoad.png',
-    title: '211',
-    status: true,
-    id: 1,
-  },
-  {
-    link: 'https://pepelsbey.net/pres/web-in-curves/pictures/hypnotoad.png',
-    title: '211',
-    status: true,
-    id: 1,
-  },
-  {
-    link: 'https://pepelsbey.net/pres/web-in-curves/pictures/hypnotoad.png',
-    title: '211',
-    status: true,
-    id: 1,
-  },
-  {
-    link: 'https://pepelsbey.net/pres/web-in-curves/pictures/hypnotoad.png',
-    title: '211',
-    status: true,
-    id: 1,
-  },
-  ];
-  const user = req.session?.userName;
-  console.log('111111', allCard);
-  render(Main, { allCard, user }, res);
-});
+route.get('/', renderHome);
 
 module.exports = route;
