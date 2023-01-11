b2cbc12d92de5ac2d70d0fc91a408ff369d95e03
Max_V <mdzen87@gmail.com>
2023-01-11 12:55:34 +0300

    добавлено изменение карточки

--- src/routes/cabinetRoutes.js
+++ src/routes/cabinetRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { renderCabinet, postCabinet, deleteCabinet } = require('../controllers/cabinetControllers');
+const { renderCabinet, postCabinet, deleteCabinet, changeCabinet, saveCabinet } = require('../controllers/cabinetControllers');
 
 router.get('/', renderCabinet);
 router.post('/', postCabinet);
 router.get('/delete/:id', deleteCabinet);
+router.get('/change/:id', changeCabinet);
+router.post('/save/:id', saveCabinet);
 
 module.exports = router;
--- src/views/Onecard.jsx
+++ src/views/Onecard.jsx
@@ -1,29 +1,28 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-function Cabinet({ user, userCards }) {
+function Onecard({ user, oneCard }) {
   return (
     <Layout user={user}>
       <div className="cabinetMine">
         <div>
-          <h2>Добавить карточку на продажу</h2>
           <div className="cabitetInput">
-            <form method="POST" action="/cabinet" className="cabinetForm">
+            <form method="POST" action={`/cabinet/save/${oneCard.id}`} className="cabinetForm">
               <div className="mb-3">
                 <label htmlFor="exampleInputTitle" className="form-label">Наименование</label>
-                <textarea type="title" name="title" className="form-control" id="exampleInputTitle" />
+                <textarea type="title" name="title" className="form-control" id="exampleInputTitle" placeholder="Наименование" value={oneCard.title}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="exampleInputCost" className="form-label">Цена</label>
-                <textarea type="cost" name="cost" className="form-control" id="exampleInputCost" />
+                <textarea type="cost" name="cost" className="form-control" id="exampleInputCost" value={oneCard.cost}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="exampleInputURL" className="form-label">Ссылка на обложку</label>
-                <textarea type="link" name="link" className="form-control" id="exampleInputURL" />
+                <textarea type="link" name="link" className="form-control" id="exampleInputURL" value={oneCard.link}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="exampleInputURL" className="form-label">Описание</label>
-                <textarea type="description" name="description" className="form-control" id="exampleInputDescription" />
+                <textarea type="description" name="description" className="form-control" id="exampleInputDescription" value={oneCard.description}/>
               </div>
               <label htmlFor="exampleInputURL" className="form-label">Состояние</label>
               <select name="condition" className="form-select" aria-label="Default select example">
@@ -31,31 +30,19 @@ function Cabinet({ user, userCards }) {
                 <option value="Хорошее">Хорошее</option>
                 <option value="Удовлетворительное">Удовлетворительное</option>
                 <option value="Плохое">Плохое</option>
-
               </select>
-              <button type="submit" className="btn btn-primary">Загрузить</button>
+              <button type="submit" className="btn btn-primary">Сохранить</button>
             </form>
           </div>
         </div>
 
         <div className="cabinetCards">
           <div className="row row-cols-1 row-cols-md-2 g-4">
-
-            {userCards.map((card) => (
               <div className="col">
                 <div className="card">
-                  <img src={card.link} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <p className="card-text">Наименование:{card.title}</p>
-                    <p className="card-text">Цена:{card.cost}</p>
-                    <p className="card-text">Состояние:{card.condition}</p>
-                    <a style={{ marginTop: 20, marginRight: 20 }} href={`/cabinet/change/${card.id}`} className="btn btn-warning">Редактировать</a>
-                    <a style={{ marginTop: 20, marginRight: 20 }} href={`/cabinet/delete/${card.id}`} className="btn btn-warning">Удалить</a>
-                  </div>
+                  <img src={oneCard.link} className="card-img-top" alt="..." />
                 </div>
               </div>
-            ))}
-
           </div>
         </div>
       </div>
@@ -63,4 +50,4 @@ function Cabinet({ user, userCards }) {
   );
 }
 
-module.exports = Cabinet;
+module.exports = Onecard;
