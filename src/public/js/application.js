64b2683cb51f74f301f85778c115183ef932e009
Khramklen <khramklen@gmail.com>
2023-01-12 11:16:28 +0300

    start branch

--- src/public/js/application.js
+++ src/public/js/application.js
@@ -45,23 +45,3 @@ document.querySelector('.search').oninput = function () {
     });
   }
 };
-
-
-// document.querySelector('.search').oninput = function () {
-//   const value = this.value.trim();
-//   const searchCard = document.querySelectorAll('.cardAll div');
-//   if (searchCard !== '') {
-//     searchCard.forEach((element) => {
-//       if (element.innerText.search(value) === -1) {
-//         element.classList.add('hide');
-//       } else {
-//         element.classList.remove('hide');
-//       }
-//     });
-//   } else {
-//     searchCard.forEach((element) => {
-//       element.classList.remove('hide');
-//     });
-//   }
-// };
-
