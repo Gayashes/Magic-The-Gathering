9b8056ed6cdb61987f9c6e80b99e2c40d4d8fe65
Max_V <mdzen87@gmail.com>
2023-01-12 18:09:21 +0300

    исправлен фетч удаления из корзины

--- src/public/js/application.js
+++ src/public/js/application.js
@@ -1,21 +1,27 @@
 /* eslint-disable camelcase */
 const ordBtn = document.querySelector('#orderBtn');
+const btnBuy = document.querySelector('#cardForm');
+const delBtn = document.querySelector('#basketcardForm');
+
+// СКРИПТ ОТПРАВКИ ПИСЬМА
+
 console.log(ordBtn);
 ordBtn?.addEventListener('click', (event) => {
-  
   alert('Письмо о Вашем заказе сформировано и отправлено продавцу')
   console.log(event);
 });
 
-const btnBuy = document.querySelector('#cardForm');
-const delBtn = document.querySelector('#basketcardForm');
+// ФЕТЧ УДАЛЕНИЯ КАРТОЧКИ ИЗ КОРЗИНЫ
+
 // console.log(delBtn);
 delBtn?.addEventListener('click', async (e) => {
+  
   // const val = e.target.classlist.contains('busketdelete');
   const card_id = e.target.id;
   // console.log('val', val);
   if (e.target.tagName === 'BUTTON') {
-    // console.log('@@@@@@@@@', e.target.id);
+    console.log('@@@@@@@@@', e.target);
+    e.target.closest('.maincardAll').remove();
     await fetch(`/basket/delete/${card_id}`, {
       method: 'POST',
     });
@@ -23,6 +29,9 @@ delBtn?.addEventListener('click', async (e) => {
   // const user_id = e.target.dataset.userid;
   // console.log(val);
 });
+
+// ФЕТЧ ДОБАВЛЕННИЯ КАРТОЧКИ В КОРЗИНУ ИЗ МЭЙНА
+
 btnBuy?.addEventListener('click', async (event) => {
   const card_id = event.target.id;
   const user_id = event.target.dataset.userid;
